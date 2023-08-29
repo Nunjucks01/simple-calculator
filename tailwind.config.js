2d63c7df6c94271f3f9a7807a2fba8b014a6f88c
Bartosz Solecki <125580157+Nunjucks01@users.noreply.github.com>
2023-08-29 19:40:58 +0200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: [],
+  theme: {
+    screens: {
+      sm: "480px",
+    },
+    extend: {
+      fontFamily: {
+        default: ["League Spartan"],
+      },
+      fontSize: {
+        default: "2rem",
+      },
+      screens: {
+        xs: "375px",
+      },
+    },
+  },
+  plugins: [],
+};
